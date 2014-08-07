684d66c4798fc2a8af0619b3dcee0b039ce5b4fa
Mattias Erming <mattias@mattiaserming.com>
2014-08-07 15:27:31 +0000

    Move prompt style to .bash_aliases

--- bash_aliases
+++ bash_aliases
@@ -1,3 +1,5 @@
+PS1='\W $ '
+
 alias t1='tree -L 1'
 alias t2='tree -L 2'
 alias t3='tree -L 3'
--- install.sh
+++ install.sh
@@ -14,19 +14,11 @@ if [ ! -d "$pwd/vimrc" ]; then
 	git clone http://github.com/erming/vimrc "$pwd/vimrc"
 fi
 
-# Install vimrc.
-# The install will create your ~/.vim/ and ~/.vimrc
+# Install vimrc
+# The install will create ~/.vim/ and ~/.vimrc
 bash "$pwd/vimrc/install.sh"
 
 cp "$pwd/bash_aliases" ~/.bash_aliases
 cp "$pwd/gitconfig" ~/.gitconfig
 
-# Force reload.
-source ~/.bash_aliases
-
-# Change prompt style
-f=~/.bashrc
-if ! grep -q "PS1='\W $ '" $f; then
-	echo "PS1='\W $ '" >> $f
-	source $f
-fi
+exec bash
