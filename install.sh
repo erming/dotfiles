82aeb065099c876d31de524603351e8fd06dce66
Mattias Erming <mattias@mattiaserming.com>
2015-07-09 08:25:01 -0700

    Move files

--- install.sh
+++ install.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
+
 pwd=`dirname $0`
 
 sudo apt-get install -y \
-  git \
-  screen \
-  tree \
-  unzip \
-  vim \
-  wget
+	git \
+	screen \
+	tree \
+	unzip \
+  	vim \
+  	wget
+
+cp "$pwd/.bash_aliases" ~/.bash_aliases
+cp "$pwd/.bash_profile" ~/.bash_profile
+cp "$pwd/.gitconfig" ~/.gitconfig
 
 if [ ! -d "$pwd/vimrc" ]; then
-  git clone http://github.com/erming/vimrc "$pwd/vimrc"
+	git clone http://github.com/erming/vimrc "$pwd/vimrc"
 fi
 
-# Install vimrc
 bash "$pwd/vimrc/install.sh"
-
-cp "$pwd/files/bash_aliases" ~/.bash_aliases
-cp "$pwd/files/bash_profile" ~/.bash_profile
-cp "$pwd/files/gitconfig" ~/.gitconfig
-
-# Force reload
-function load() {
-  source ~/.bash_profile
-}
-load
-unset load
